f57eef73a1474b4999286625074ec706d9f9c111
savke30 <43907639+savke30@users.noreply.github.com>
2018-12-10 21:20:16 +0100

    Promjena preko github app

--- DomZdravlja/Form1.Designer.cs
+++ DomZdravlja/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(474, 328);
+            this.ClientSize = new System.Drawing.Size(1000, 328);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
