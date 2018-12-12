eadf4820e4aee106293771b8063c6f5c4a5d3ddd
sandra anicic <sandra.anicic2@outlook.com>
2018-12-12 14:55:19 +0100

    Promjena nameSpace i pocetak ucitavanja liste Zaposlenih

--- DomZdravlja/Helpers/SqlHelper.cs
+++ DomZdravlja/Helpers/SqlHelper.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace KonekcijaNaBazu
+namespace DomZdravlja
 {
     //===============================================================================
     // Microsoft Data Access Application Block for .NET
