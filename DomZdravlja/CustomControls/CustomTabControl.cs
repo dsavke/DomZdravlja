4a58c86a4c23b44d998322815ac81d5288e93b11
dejan.savanovic <dejansavanovic13@gmail.com>
2018-12-12 15:25:57 +0100

    Opet nisam napravio granu pa moram opet commit.

---
+++ DomZdravlja/CustomControls/CustomTabControl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DomZdravlja.CustomControls
+{
+    public class CustomTabControl:TabControl
+    {
+    }
+}
