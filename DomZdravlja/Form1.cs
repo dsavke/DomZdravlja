e4ebf13cd0b9c3f1fea6285917249a244ba96205
srdjan.tomic <srdjantomic44@gmail.com>
2018-12-14 12:28:20 +0100

    Ispravke koda

--- DomZdravlja/Form1.cs
+++ DomZdravlja/Form1.cs
@@ -182,7 +182,7 @@ namespace DomZdravlja
             while (dataReader.Read())
             {
                 PropertyKartonDijagnoza pomKartonDijagnoza = new PropertyKartonDijagnoza();
-                pomKartonDijagnoza.KartonDijagnozaID = (int)dataReader["PacijentID"];
+                pomKartonDijagnoza.KartonDijagnozaID = (int)dataReader["KartonDijagnozaID"];
                 pomKartonDijagnoza.KartonID = (int)dataReader["KartonID"];
                 pomKartonDijagnoza.DijagnozaID = (int)dataReader["DijagnozaID"];
                 listaKartonDijagnoza.Add(pomKartonDijagnoza);
